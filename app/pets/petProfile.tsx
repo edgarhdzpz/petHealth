5252adc12fceda177dd997c86c0d536ff9cda38a
LytzyMorales <lytzy.morales23@gmail.com>
2024-11-13 23:41:27 -0600

    implementé las pantallas

---
+++ app/pets/petProfile.tsx
@@ -0,0 +1,6 @@
+import PetProfileView from "../../components/features/pets/aplication/screens/petProfileView";
+
+
+export default function PetProfileScreen() {
+    return <PetProfileView route={undefined} navigation={undefined} />
+}
